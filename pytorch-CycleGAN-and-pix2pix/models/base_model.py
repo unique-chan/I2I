7d15dd5e4081913776300d1127ee6e37e56d2155
unique-chan <yechankim@gm.gist.ac.kr>
2025-05-02 18:53:12 +0900

    +

--- pytorch-CycleGAN-and-pix2pix/models/base_model.py
+++ pytorch-CycleGAN-and-pix2pix/models/base_model.py
@@ -85,7 +85,13 @@ class BaseModel(ABC):
             self.schedulers = [networks.get_scheduler(optimizer, opt) for optimizer in self.optimizers]
         if not self.isTrain or opt.continue_train:
             load_suffix = 'iter_%d' % opt.load_iter if opt.load_iter > 0 else opt.epoch
-            self.load_networks(load_suffix)
+            # Yechan modified ->
+            try:
+                self.load_networks(load_suffix)
+            except Exception as e:
+                print(e)
+                print(f'Import the given weight path, {opt.weight_path}!!!')
+                self.load_networks(opt.weight_path)
         self.print_networks(opt.verbose)
 
     def eval(self):
@@ -173,7 +179,7 @@ class BaseModel(ABC):
         else:
             self.__patch_instance_norm_state_dict(state_dict, getattr(module, key), keys, i + 1)
 
-    def load_networks(self, epoch):
+    def load_networks(self, epoch, load_path=None):
         """Load all the networks from the disk.
 
         Parameters:
@@ -182,7 +188,8 @@ class BaseModel(ABC):
         for name in self.model_names:
             if isinstance(name, str):
                 load_filename = '%s_net_%s.pth' % (epoch, name)
-                load_path = os.path.join(self.save_dir, load_filename)
+                if not load_path:
+                    load_path = os.path.join(self.save_dir, load_filename)
                 net = getattr(self, 'net' + name)
                 if isinstance(net, torch.nn.DataParallel):
                     net = net.module
